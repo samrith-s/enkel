3c20b430f8dce1ba1941ba270bb7cd5bd3dec4ab
Samrith Shankar <samrith@thetotem.co>
2019-07-29 01:35:01 +0200

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+// Provider
+export { ThemeProvider } from 'lib/ThemeProvider';
+
 // Wrapping components
-export { default as Container } from 'components/Container';
-export { default as Flex } from 'components/Flex';
+export { Container } from 'components/Container';
+export { Flex } from 'components/Flex';
 
 // Generic components
-export { default as Heading } from 'components/Heading';
-export { default as Anchor } from 'components/Anchor';
-export { default as Paragraph } from 'components/Paragraph';
+export { Heading } from 'components/Heading';
+export { Anchor } from 'components/Anchor';
+export { Paragraph } from 'components/Paragraph';
 // export { default as Button } from 'components/Button';
 
 // Form components
-export { default as Form } from 'components/Form';
+export { Form } from 'components/Form';
 // export { default as Input } from 'components/Input';
-export { default as Select } from 'components/Select';
+export { Select } from 'components/Select';
 // export { default as Checkbox } from 'components/Checkbox';
 // export { default as Radio } from 'components/Radio';
