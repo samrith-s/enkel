49c97e6730e3f450aa995b50283aad063e9d87bc
Samrith Shankar <samrith@thetotem.co>
2019-07-29 01:32:08 +0200

    Added theme default

---
+++ src/defaults/theme.ts
@@ -0,0 +1,17 @@
+import { ThemeInterface } from 'interfaces/theme-provider.interface';
+
+export const theme: ThemeInterface = {
+    colors: {
+        dark: '#212026',
+        light: '#f9f9f9',
+        primary: '#f9703c',
+        secondary: '#21002d',
+        yellow: '#ffe12c'
+    },
+    variables: {
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '5px',
+        maxWidth: '1200px'
+    }
+};
