50441cc4cb0a751333365b67d964ff1a52432c88
Samrith Shankar <samrith@thetotem.co>
2019-07-28 18:23:33 +0200

    Imported files into index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { default as Container } from 'components/Container';
+export { default as Flex } from 'components/Flex';
+
+export { default as Heading } from 'components/Heading';
+export { default as Anchor } from 'components/Anchor';
+export { default as Paragraph } from 'components/Paragraph';
 export { default as Span } from 'components/Span';
