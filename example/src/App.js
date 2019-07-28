a14b2a3df5f4104ed69cb421e516a6b8a821cb20
Samrith Shankar <samrith@thetotem.co>
2019-07-28 18:23:42 +0200

    Lint and use Span component as a test

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'enkel'
+import { Span } from 'enkel-ui';
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Span>Here is a Span!</Span>
+            </div>
+        );
+    }
 }
