eda1bee576b54575bbeab52dbe0d660074bece83
Samrith Shankar <samrith@thetotem.co>
2019-07-29 00:04:19 +0200

    use children in form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
 
-const Form: FunctionComponent = props => <div>Form</div>;
+const Form: FunctionComponent = props => <div>Form {props.children}</div>;
 
 export default Form;
