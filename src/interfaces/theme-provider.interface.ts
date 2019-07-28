59aa2f85f03696428b79286b50fc45e1e82702e7
Samrith Shankar <samrith@thetotem.co>
2019-07-29 01:32:02 +0200

    Added theme-provider interface

--- src/interfaces/theme-provider.interface.ts
+++ src/interfaces/theme-provider.interface.ts
@@ -1,6 +1,28 @@
-export interface ThemeInterface = {
-    colors: Object<>,
-    variables: Object<>
+import { ReactElement } from "react";
+
+export interface ThemeInterface {
+    colors: ThemeColorsInterface;
+    variables: ThemeVariablesInterface;,
 }
 
-export interface ThemeColorsInterface
+export interface ThemeColorsInterface {
+    dark?: string;
+    light?: string;
+    primary?: string;
+    secondary?: string;
+    green?: string;
+    blue?: string;
+    yellow?: string;
+    orange?: string;
+    red?: string;
+}
+export interface ThemeVariablesInterface {
+    margin?: string;
+    padding?: string;
+    borderRadius?: string;
+    maxWidth?: string;
+}
+
+export interface ThemeProviderInterface extends ThemeInterface {
+    children: ReactElement
+}
