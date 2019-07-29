4aa706b6f9f2c45bdd4444281a4dfa021c2106f3
Samrith Shankar <samrith@thetotem.co>
2019-07-29 22:02:20 +0200

    Update anchor component with internals

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,14 +1,12 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 
 import { AnchorProps } from 'interfaces/anchor.interface';
+
+import RenderAppropriate from 'internals/RenderAppropriate';
+import { AnchorComponent } from 'styles/anchor.styles';
 
 export const Anchor: FunctionComponent<AnchorProps> = ({
     children,
     component: Component,
     ...rest
-}) =>
-    Component ? (
-        <Component {...rest}>{children}</Component>
-    ) : (
-        <a {...rest}>{children}</a>
-    );
+}) => RenderAppropriate(AnchorComponent, rest, children, Component);
