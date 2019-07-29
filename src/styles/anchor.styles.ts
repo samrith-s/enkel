2e7f3041b7681170d073607338a79cd9c4019743
Samrith Shankar <samrith@thetotem.co>
2019-07-29 23:12:05 +0200

    Updated anchor style

--- src/styles/anchor.styles.ts
+++ src/styles/anchor.styles.ts
@@ -1,3 +1,13 @@
-import styled from 'styled-components';
+import styled from 'styled';
 
-export const AnchorComponent = styled.a``;
+export const AnchorStyle = styled.a`
+    color: ${props => props.theme.colors.primary};
+    text-decoration: none;
+    border-bottom: 1px dotted transparent;
+    cursor: pointer;
+    transition: all 0.5s ease;
+
+    &:hover {
+        border-color: ${({ theme: { colors } }) => colors.primary};
+    }
+`;
