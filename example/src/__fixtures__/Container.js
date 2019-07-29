e58820868ca3b25101b431a26cd2fd15d870fce4
Samrith Shankar <samrith@thetotem.co>
2019-07-30 00:08:41 +0200

    Add container fixture

---
+++ example/src/__fixtures__/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'enkel-ui';
+
+import AsFixture from '../AsFixture';
+
+export default (
+    <AsFixture
+        fixture={Container}
+        isFluid={false}
+        noPadding={false}
+        maxWidth="100%"
+    >
+        This is a container. You can play around with the props, and pass custom
+        styles!
+    </AsFixture>
+);
