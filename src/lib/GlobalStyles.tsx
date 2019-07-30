7403bfc1e7ef5b8c21cd78c07cc1cca979673f79
Samrith Shankar <samrith@thetotem.co>
2019-07-30 03:09:28 +0200

    Added root style to global

--- src/lib/GlobalStyles.tsx
+++ src/lib/GlobalStyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled';
 export const GlobalStyles = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700&display=swap');
 
-    html, body {
+    html, body, #root {
         width: 100%;
         height: 100%;
         margin: 0;
@@ -11,6 +11,7 @@ export const GlobalStyles = createGlobalStyle`
         color: ${({ theme: { colors } }) => colors.dark};
         font-family: 'Nanum Gothic', Arial, Helvetica, sans-serif;
         background: ${({ theme: { colors } }) => colors.light};
+        font-size: ${({ theme: { variables } }) => variables.baseFontSize};
     }
 
     * {
