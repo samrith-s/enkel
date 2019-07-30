c61c30252e049be69ba37f6b94286b7677612e23
Samrith Shankar <samrith@thetotem.co>
2019-07-30 21:52:36 +0200

    Added ComponentCommons

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,10 @@
-import { FunctionComponent, ReactComponentElement } from 'react';
+import { FunctionComponent, ReactComponentElement, ReactNode } from 'react';
 
 export interface EnkelComponent<T> extends FunctionComponent<T> {
     Style: ReactComponentElement;
+}
+
+export interface ComponentCommons {
+    children?: ReactNode;
+    component?: Function;
 }
