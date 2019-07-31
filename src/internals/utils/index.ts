f9880a2a39865858242bc7be68a79318bb886e21
Samrith Shankar <samrith@thetotem.co>
2019-07-31 22:19:35 +0200

    Add util to group display name

---
+++ src/internals/utils/index.ts
@@ -0,0 +1,22 @@
+import { FunctionComponent } from 'react';
+
+export function groupDisplayName(
+    componentGroup: string,
+    delimiter: string = '.'
+): Function {
+    return function applyDisplayNameToComponent(
+        originalComponent: FunctionComponent,
+        sanitize: boolean = true
+    ): FunctionComponent {
+        let componentName =
+            originalComponent!.displayName || originalComponent!.name;
+
+        if (sanitize) {
+            componentName = componentName!.replace(componentGroup, '');
+        }
+
+        originalComponent!.displayName =
+            componentGroup + delimiter + componentName;
+        return originalComponent;
+    };
+}
