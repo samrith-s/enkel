a50fd684c29d7fa34989fbf65676d7d801aaaaec
Samrith Shankar <samrith@thetotem.co>
2019-07-31 22:20:21 +0200

    [WIP] Added styles for various aspects of the card

--- src/styles/display/card.styles.ts
+++ src/styles/display/card.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled';
-import { lighten } from 'polished';
+import { lighten, padding } from 'polished';
 
 import {
     CardStyleProps,
@@ -9,19 +9,37 @@ import {
 export const CardStyle = styled.div<CardStyleProps>`
     display: flex;
     flex-flow: column;
+    margin: ${({ theme: { variables } }) => variables.margin};
     background: ${({ theme: { colors } }) => colors.light};
-    border: 1px solid ${({ theme: { colors } }) => lighten(0.5, colors.darken)}
+    border: 1px solid ${({ theme: { colors } }) => lighten(0.8, colors.dark)}
     border-radius: ${({ theme: { variables } }) => variables.borderRadius};
 `;
 
 export const CardTitleStyle = styled.div<CardTitleStyleProps>`
-    text-align: left;
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: ${({ align }) => align};
+    min-height: 34px;
+    margin: 0 0 ${({ theme: { variables } }) => variables.margin};
+    padding: ${({ theme: { variables } }) =>
+        padding(0, variables.padding, variables.padding)};
+    font-weight: bold;
+    border-bottom: 1px solid
+        ${({ theme: { colors } }) => lighten(0.75, colors.dark)};
 `;
 
 export const CardBodyStyle = styled.div<CardTitleStyleProps>`
-    text-align: left;
+    flex-shrink: 1;
 `;
 
 export const CardFooterStyle = styled.div<CardTitleStyleProps>`
-    text-align: left;
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    min-height: 34px;
+    margin: ${({ theme: { variables } }) => variables.margin} 0 0;
+    padding: ${({ theme: { variables } }) => variables.padding} 0 0;
+    border-top: 1px solid
+        ${({ theme: { colors } }) => lighten(0.75, colors.dark)};
 `;
