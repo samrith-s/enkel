741f9a0fc3cc0e219cb247e504a161bbf4191765
Samrith Shankar <samrith@thetotem.co>
2019-08-07 13:41:16 +0200

    Added StyleInterface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,11 @@
 import { FunctionComponent, ReactNode } from 'react';
 
 export interface EnkelComponent<T> extends FunctionComponent<T> {
-    Style?: ReactNode;
+    Style?: ReactNode | EnkelStyleInterface;
+}
+
+interface EnkelStyleInterface {
+    [type: string]: ReactNode;
 }
 
 export interface ComponentCommons {
