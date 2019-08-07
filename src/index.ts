0530b4a68dcf7cd23a6b5eec6e1eb42697f0f0d2
Samrith Shankar <samrith@thetotem.co>
2019-08-07 14:40:35 +0200

    Exported Button

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export { Paragraph } from 'components/typography/Paragraph';
 // export { Form } from 'components/form/Form';
 // export { default as Input } from 'components/Input';
 export { Select } from 'components/form/Select';
+export { Button } from 'components/form/Button';
 // export { default as Checkbox } from 'components/Checkbox';
 // export { default as Radio } from 'components/Radio';
 
