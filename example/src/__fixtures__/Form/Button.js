c776b90aea5dc8b28730c5c01f7cb6d459ae09e1
Samrith Shankar <samrith@thetotem.co>
2019-12-09 18:08:46 +0530

    Wrap fixture in div

--- example/src/__fixtures__/Form/Button.js
+++ example/src/__fixtures__/Form/Button.js
@@ -3,12 +3,14 @@ import { ThemeProvider, Button } from 'enkel-ui';
 
 export default (
     <ThemeProvider>
-        <Button>Default</Button>
-        <Button type="success">Success</Button>
-        <Button type="info">Info</Button>
-        <Button type="warning">Warning</Button>
-        <Button type="danger">Danger</Button>
-        <Button type="light">Light</Button>
-        <Button type="dark">Dark</Button>
+        <div style={{ padding: 10 }}>
+            <Button>Default</Button>
+            <Button type="success">Success</Button>
+            <Button type="info">Info</Button>
+            <Button type="warning">Warning</Button>
+            <Button type="danger">Danger</Button>
+            <Button type="light">Light</Button>
+            <Button type="dark">Dark</Button>
+        </div>
     </ThemeProvider>
 );
