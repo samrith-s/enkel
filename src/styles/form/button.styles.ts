f10404df23b42b849e00c4ff0268fd9ce40693e4
Samrith Shankar <samrith@thetotem.co>
2019-12-09 19:02:29 +0530

    Update button styles

--- src/styles/form/button.styles.ts
+++ src/styles/form/button.styles.ts
@@ -1,30 +1,30 @@
-import styled from 'styled';
-import { padding, darken } from 'polished';
+import styled, { css } from "styled";
+import { padding, darken } from "polished";
 
-import { ButtonInterface } from 'interfaces/form/button.interface';
-import { getColorFromKey } from 'utils/getColorFromKey';
+import { ButtonInterface } from "interfaces/form/button.interface";
+import { getColorFromKey } from "utils/getColorFromKey";
 
 export const ButtonStyle = styled.button<ButtonInterface>`
     display: flex;
-    align-items: ${({ alignItems }) => alignItems || 'center'};
-    justify-content: ${({ justifyContent }) => justifyContent || 'center'};
+    align-items: ${({ alignItems }) => alignItems || "center"};
+    justify-content: ${({ justifyContent }) => justifyContent || "center"};
     height: 34px;
     ${({ theme: { variables } }) => padding(0, +variables.padding * 1.5)}
     ${({ type, theme: { colors } }) => {
         const { color, background } = getColorFromKey(type);
 
-        return `
+        return css`
             color: ${colors[color]};
             background: ${colors[background]};
-            border: 1px solid ${darken(0.05, colors[background])};
-            
-            &:hover, &:active {
+
+            &:hover,
+            &:active {
                 background: ${darken(0.15, colors[background])};
-                border: 1px solid ${darken(0.2, colors[background])};
             }
         `;
     }};
     font-size: inherit;
+    border: 0;
     border-radius: ${({ theme: { variables } }) => variables.borderRadius};
     outline: 0;
     cursor: pointer;
